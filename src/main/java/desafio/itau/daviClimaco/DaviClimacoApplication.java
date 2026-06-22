cee21e73e5d844ff03ee1013566ff63fc3af92a1
daviClimaco <davibomfimclimaco@gmail.com>
2026-06-22 20:57:38 -0300

    Iniciado Repositorio

---
+++ src/main/java/desafio/itau/daviClimaco/DaviClimacoApplication.java
@@ -0,0 +1,13 @@
+package desafio.itau.daviClimaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaviClimacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaviClimacoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/desafio/itau/daviClimaco/DaviClimacoApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.itau.daviClimaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaviClimacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
